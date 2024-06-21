57b3d5ce6c6c332d98fbae70f04b6d2944b699f1
Jack0O7 <dastan.nomad@gmail.com>
2024-06-21 16:28:16 +0500

    basic mini server

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"io"
+	"log"
+	"net/http"
+)
+
+func main() {
+	// get an engine instance
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		response, err := http.Get("http://localhost:3000/animals")
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to connect to the CRUD server"})
+			return
+		}
+		defer response.Body.Close()
+
+		body, err := io.ReadAll(response.Body)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to read body from the CRUD server"})
+			return
+		}
+
+		c.Data(http.StatusOK, "application/json", body)
+	})
+
+	// run the server
+	err := r.Run(":3000")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
